cc6ef240011d281517c0d754b715b8e23d15a199
Victor Perin <me@victorperin.ninja>
2018-06-10 23:58:32 -0300

    tests(api-answers/api): should return 200 on /healthcheck

---
+++ api-answers/src/api.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+
+describe('API', () => {
+  const api = require('./api');
+  it('should return 200 on /healthcheck', async () => {
+    const server = api({ port: null });
+
+    const { body, status } = await request(server)
+      .get('/healthcheck');
+
+    expect(status).toBe(200);
+    expect(body).toHaveProperty('uptime');
+  });
+})
