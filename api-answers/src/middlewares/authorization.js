cf9d2849af92be4b5382e35d939bde1d8e45041f
Victor Perin <me@victorperin.ninja>
2018-06-16 16:42:54 -0300

    refactor(api-answers/middewares/authorization): removing jwt check (done in api-gateway)

--- api-answers/src/middlewares/authorization.js
+++ api-answers/src/middlewares/authorization.js
@@ -1,23 +1,3 @@
 const jwt = require('koa-jwt');
-const jwksRsa = require('jwks-rsa');
 
-const { authorization: authorizationConfig } = require('../config');
-const {
-  host,
-  audience,
-  issuer,
-} = authorizationConfig;
-
-const jwtSecretConfig = {
-  cache: true,
-  rateLimit: true,
-  jwksRequestsPerMinute: 2,
-  jwksUri: `${host}/.well-known/jwks.json`
-};
-
-module.exports = () => jwt({
-  secret: jwksRsa.koaJwtSecret(jwtSecretConfig),
-  audience,
-  issuer,
-  algorithms: [ 'RS256' ]
-});
+module.exports = () => jwt();
