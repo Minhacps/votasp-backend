2adf344e4e59fe90ea09c3e408fe230c32cf41bf
Victor Perin <me@victorperin.ninja>
2018-06-30 18:07:35 -0300

    fix(api-questions): cors error

--- api-questions/src/middlewares/index.js
+++ api-questions/src/middlewares/index.js
@@ -1,10 +1,12 @@
 const compose = require('koa-compose');
+const cors = require('@koa/cors');
 
 const koaRespond = require('./koa-respond');
 const loadRouteFiles = require('./load-route-files');
 
 module.exports = async () =>
     compose([
+      cors(),
       koaRespond(),
       ...await loadRouteFiles(),
     ]);
