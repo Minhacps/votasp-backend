f78420e03e9a817cbc0658bf0edba0da2f95e509
Victor Perin <me@victorperin.ninja>
2018-07-01 18:10:21 -0300

    refactor(api-answers/models): moving model from domains folder

--- api-answers/src/domains/answers/model.js
+++
@@ -1,7 +0,0 @@
-const Mongoose = require('mongoose');
-const Joigoose = require('../../infrastructure/joigoose');
-const Answers = require('../../schemas/answers');
-
-const schema = new Mongoose.Schema( Joigoose.convert(Answers) );
-
-module.exports = Mongoose.model('Answers', schema);
