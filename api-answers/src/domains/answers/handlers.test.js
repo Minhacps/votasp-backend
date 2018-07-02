e059d62c72433714f6622053f925e43c58c0f4d0
Victor Perin <me@victorperin.ninja>
2018-07-01 21:05:03 -0300

    tests(api-answers/domains/answers/handlers)

---
+++ api-answers/src/domains/answers/handlers.test.js
@@ -0,0 +1,38 @@
+const mockingoose = require('mockingoose').default;
+
+const _SampleAnswers = {
+  userId: 'biru|laibe12345',
+  answers: [
+    { questionId: 1, answer: 'discordo' },
+    { questionId: 2, answer: 'concordo' },
+    { questionId: 3, answer: 'concordo_plenamente' },
+  ],
+};
+
+describe('Domains/Answers/Handlers', () => {
+  describe('listUserAnswers', () => {
+    const { listUserAnswers } = require('./handlers');
+
+    it('should list candidate answer', async () => {
+      mockingoose.CandidateAnswers.toReturn(_SampleAnswers, 'findOne');
+
+      const context = {
+        state: { user: {
+          isCandidate: true,
+          sub: 'biru|laibe12345',
+        } },
+        ok: jest.fn(),
+        notFound: jest.fn(),
+        internalServerError: jest.fn(),
+      };
+
+      await listUserAnswers(context);
+
+      expect(context.ok.mock.calls.length).toBe(1);
+      expect(context.notFound.mock.calls.length).toBe(0);
+      expect(context.internalServerError.mock.calls.length).toBe(0);
+      expect(context.ok.mock.calls[0][0].toObject()).toMatchObject(_SampleAnswers);
+    });
+
+  });
+})
