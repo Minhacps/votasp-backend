2dc72f4b847b4e11cfaf6517b4907116393f3bec
Victor Perin <me@victorperin.ninja>
2018-07-02 14:30:07 -0300

    test(api-answers/Domains/Answers/Handlers): should list voter Answers

--- api-answers/src/domains/answers/handlers.test.js
+++ api-answers/src/domains/answers/handlers.test.js
@@ -34,5 +34,26 @@ describe('Domains/Answers/Handlers', () => {
       expect(context.ok.mock.calls[0][0].toObject()).toMatchObject(_SampleAnswers);
     });
 
+    it('should list voter Answers', async () => {
+      mockingoose.VoterAnswers.toReturn(_SampleAnswers, 'findOne');
+
+      const context = {
+        state: { user: {
+          isCandidate: false,
+          sub: 'biru|laibe12345',
+        } },
+        ok: jest.fn(),
+        notFound: jest.fn(),
+        internalServerError: jest.fn(),
+      };
+
+      await listUserAnswers(context);
+
+      expect(context.ok.mock.calls.length).toBe(1);
+      expect(context.notFound.mock.calls.length).toBe(0);
+      expect(context.internalServerError.mock.calls.length).toBe(0);
+      expect(context.ok.mock.calls[0][0].toObject()).toMatchObject(_SampleAnswers);
+    });
+
   });
 })
