5e86c213d84cb2baab08ac7134be19b23cb99901
Victor Perin <me@victorperin.ninja>
2018-07-02 14:32:14 -0300

    refactor(api-answers/domains/answers/handlers): _genericResponseOrNotFound

--- api-answers/src/domains/answers/handlers.js
+++ api-answers/src/domains/answers/handlers.js
@@ -6,14 +6,16 @@ const _genericInternalErrorGenerator = (context) =>
     context.internalServerError('Something went wrong');
   };
 
+const _genericResponseOrNotFound = (context) =>
+  (response) =>
+    response ?
+      context.ok(response) :
+      context.notFound();
+
 const _listAnswersGenerator = (type = 'Voter') =>
   (context) =>
     Answers[type].findOne({ userId: context.state.user.sub })
-      .then( (response) =>
-        response ?
-          context.ok(response) :
-          context.notFound()
-      )
+      .then( _genericResponseOrNotFound(context) )
       .catch( _genericInternalErrorGenerator(context) );
 
 const listUserAnswers = (context) =>
